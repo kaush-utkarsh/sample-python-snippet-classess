e5b11647ef87b28224b6db9c7ee1b4ce1453eb0e
Utkarsh Kaushik <utkarsh5929@gmail.com>
2018-12-27 11:59:38 +0530

    removing snippets temporarily

--- basic general/23AsciiValue.py
+++
@@ -1,9 +0,0 @@
-# Program to find the ASCII value of the given character
-
-# Change this value for a different result
-c = 'p'
-
-# Uncomment to take character from user
-#c = input("Enter a character: ")
-
-print("The ASCII value of '" + c + "' is",ord(c))
